8157c0efee0ae9a402212a35aaa9b473c4552b58
Amr Fawaz <amr.abdelhakim.fawaz+75@gmail.com>
2024-07-09 20:33:43 +0300

    Initial Commit

---
+++ WordFinder/WordFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordFinderApp.swift
+//  WordFinder
+//
+//  Created by AmrFawaz on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
